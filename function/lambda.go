43fd355ce0b10fe2da4fab997c4b799e9dab7f8a
Kevin Webster <kevin@kevinwebster.me>
2020-09-09 19:46:41 -0700

    First commit

---
+++ function/lambda.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Response events.APIGatewayProxyResponse
+
+func Handler(ctx context.Context) (Response, error) {
+	return Response{StatusCode: 200}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/rabidpraxis/a-lambda
+
+go 1.15
+
+require github.com/aws/aws-lambda-go v1.19.1
+
+require github.com/aws/aws-sdk-go v1.34.17
